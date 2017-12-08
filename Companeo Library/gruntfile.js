4b07e20adb950158c32d5facbad0dbcabd460b2e
tdeloris <tdeloris@companeo.com>
2017-12-08 08:40:47 +0100

    changed the main folder name

---
+++ Companeo Library/gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'scss',
+                    src: ['*.scss'],
+                    dest: 'css/',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true,
+            },
+            styles: {
+                files: 'scss/*.scss',
+                tasks: ['sass']
+            }
+        }
+
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default',['sass'],'watch')
+
+}
